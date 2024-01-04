3f856b0e32a16b67152a0a38a0160c6ed74528c3
akshat <akshatmittal.0007@gmail.com>
2024-01-04 20:19:15 +0530

    debug config

--- scripts/sum_script.py
+++ scripts/sum_script.py
@@ -1,11 +1,23 @@
 import sys
 
+import pdb;
+import debugpy;
+# debugpy.listen(8028)
+
+# debugpy.wait_for_client()
+
 input_file = snakemake.input[0]
 output_file = snakemake.output[0]
+
+
 
 with open(input_file, 'r') as file:
     numbers = [int(line.strip()) for line in file.readlines()]
 total = sum(numbers)
+
+# breakpoint()
+
+debugpy.breakpoint()
 
 with open(output_file, 'w') as file:
     file.write(str(total))
